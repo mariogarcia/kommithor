25def4cb3a021ad0cdf80afdfc5dbc7cb05b87b6
Mario Garcia <mario.ggar@gmail.com>
2018-10-22 00:56:01 +0200

    Add sse basic example to start from

--- src/main/kotlin/commithor/Application.kt
+++ src/main/kotlin/commithor/Application.kt
@@ -7,14 +7,18 @@ import io.ktor.routing.route
 import io.ktor.routing.routing
 
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.response.header
 import io.ktor.response.respond
 import io.ktor.response.respondText
+import io.ktor.response.respondTextWriter
 
 import io.ktor.server.netty.Netty
 import io.ktor.server.engine.embeddedServer
 
+import io.ktor.http.HttpHeaders
 import io.ktor.http.ContentType
 import io.ktor.http.content.static
 import io.ktor.http.content.resources
@@ -25,6 +29,11 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import commithor.git.getSlackersFrom
 import java.text.SimpleDateFormat
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.channels.broadcast
+import kotlinx.coroutines.channels.ReceiveChannel
 
 /**
  * Application's main entry point
@@ -77,6 +86,48 @@ fun Application.main() {
             val credentials = UsernamePasswordCredentialsProvider(username, password)
 
             call.respond(getSlackersFrom(repository, tempDir, credentials))
+        }
+
+        get("/sse") {
+            call.respondSse(channel.openSubscription())
+        }
+    }
+}
+
+val channel = produce { // this: ProducerScope<SseEvent> ->
+            var n = 0
+            while (true) {
+                send(SseEvent("demo$n"))
+                delay(1000)
+                n++
+            }
+        }.broadcast()
+
+data class SseEvent(val data: String, val event: String? = null, val id: String? = null)
+
+val ContentTypeTextEventStream = ContentType.parse("text/event-stream")
+
+/**
+ * Method that responds an [ApplicationCall] by reading all the [SseEvent]s from the specified [events] [ReceiveChannel]
+ * and serializing them in a way that is compatible with the Server-Sent Events specification.
+ *
+ * You can read more about it here: https://www.html5rocks.com/en/tutorials/eventsource/basics/
+ */
+suspend fun ApplicationCall.respondSse(events: ReceiveChannel<SseEvent>) {
+    response.header(HttpHeaders.CacheControl, "no-cache")
+    respondTextWriter(contentType = ContentTypeTextEventStream) {
+        for (event in events) {
+            if (event.id != null) {
+                write("id: ${event.id}\n")
+            }
+            if (event.event != null) {
+                write("event: ${event.event}\n")
+            }
+            for (dataLine in event.data.lines()) {
+                write("data: $dataLine\n")
+            }
+            write("\n")
+            flush()
         }
     }
 }
